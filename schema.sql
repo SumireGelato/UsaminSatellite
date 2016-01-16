e98fd8e82376ffe885acc68043104a2c43556ba5
KillerOsaka <KillerOsaka@5e555852-faa2-0f4e-a87b-c86d43468a95>
2016-01-16 12:41:09 +0000

    git-svn-id: http://localhost:81/svn/Satelite@16 5e555852-faa2-0f4e-a87b-c86d43468a95

--- schema.sql
+++ schema.sql
@@ -58,6 +58,7 @@ CREATE TABLE songs (
 CREATE TABLE events (
   id        INT UNSIGNED   NOT NULL AUTO_INCREMENT,
   isCurrent BOOLEAN        NOT NULL,
+  isCaravan BOOLEAN        NOT NULL,
   eName     CHARACTER(255) NOT NULL,
   jName     CHARACTER(255) NOT NULL,
   begin     DATETIME       NOT NULL,
@@ -84,7 +85,7 @@ CREATE TABLE users (
 CREATE TABLE cards (
   id               INT UNSIGNED   NOT NULL AUTO_INCREMENT,
   idol_id          INT UNSIGNED   NOT NULL,
-  event_id         INT UNSIGNED   NOT NULL,
+  event_id         INT UNSIGNED,
   eName            CHARACTER(255) NOT NULL,
   jName            CHARACTER(255) NOT NULL,
   rarity           CHARACTER(5)   NOT NULL,
@@ -133,22 +134,44 @@ FOREIGN KEY (event_id)
 REFERENCES events (id);
 
 
-ALTER TABLE `idols` CHANGE `jName` `jName` VARCHAR(255) CHARACTER SET eucjpms COLLATE eucjpms_japanese_ci NOT NULL;
-ALTER TABLE `songs` CHANGE `jName` `jName` VARCHAR(255) CHARACTER SET eucjpms COLLATE eucjpms_japanese_ci NOT NULL;
-ALTER TABLE `events` CHANGE `jName` `jName` VARCHAR(255) CHARACTER SET eucjpms COLLATE eucjpms_japanese_ci NOT NULL;
-ALTER TABLE `cards` CHANGE `jName` `jName` VARCHAR(255) CHARACTER SET eucjpms COLLATE eucjpms_japanese_ci NOT NULL;
+ALTER TABLE `idols` CHANGE `jName` `jName` VARCHAR(255)
+CHARACTER SET eucjpms
+COLLATE eucjpms_japanese_ci NOT NULL;
+ALTER TABLE `songs` CHANGE `jName` `jName` VARCHAR(255)
+CHARACTER SET eucjpms
+COLLATE eucjpms_japanese_ci NOT NULL;
+ALTER TABLE `events` CHANGE `jName` `jName` VARCHAR(255)
+CHARACTER SET eucjpms
+COLLATE eucjpms_japanese_ci NOT NULL;
+ALTER TABLE `cards` CHANGE `jName` `jName` VARCHAR(255)
+CHARACTER SET eucjpms
+COLLATE eucjpms_japanese_ci NOT NULL;
 
 
 -- test data (User -> News -> Idol -> Event -> Card -> Song
 -- ---------------------------------
 INSERT INTO users (id, email, password) VALUES
-  (1,'pangster123@gmail.com', '$2a$10$j2vlpErMLsUgkCGIPv.VKO8k3T3Fa5GI4GpM01WF3sBLwsnzw2HOq');
+  (1, 'pangster123@gmail.com', '$2a$10$j2vlpErMLsUgkCGIPv.VKO8k3T3Fa5GI4GpM01WF3sBLwsnzw2HOq');
 
 INSERT INTO news (id, author_id, title, body, created, isPublished) VALUES
-  (1, 1, 'Test Post', '<h1> This is a published test post </h1>', '2014-09-17 15:33:26', TRUE),
-  (2, 1, 'Test Post 2', '<h1> This is a draft test post</h1>', '2014-09-17 15:33:26', FALSE);
+  (1, 1, 'Test Post', '<h1> This is a published test post </h1>', '2015-09-17 15:33:26', TRUE),
+  (2, 1, 'Test Post 2', '<h1> This is a draft test post</h1>', '2015-09-17 15:33:26', FALSE);
 
-INSERT INTO idols (id,eName,jName,age,height,weight,birthday,bloodType,bwh,hobbies,writingHand,horoscope,hometown,type,cv,pic1) VALUES
-  (1,'Shimamura Uzuki', '島村卯月', '17',159,45,'April 24','O','83/59/87','Long Phone Calls with friends', 'Right','Taurus', 'Tokyo', 'Cute', 'Ohashi Ayaka', ' ');
+INSERT INTO idols (id, eName, jName, age, height, weight, birthday, bloodType, bwh, hobbies, writingHand, horoscope, hometown, type, cv, pic1)
+VALUES
+  (1, 'Uzuki Shimamura', '島村卯月', '17', 159, 45, 'April 24', 'O', '83/59/87', 'Long Phone Calls with friends', 'Right',
+   'Taurus', 'Tokyo', 'Cute', 'Ayaka Ohashi', ' '),
+  (2, 'Rin Shibuya', '渋谷凜', '15', 165, 44, 'August 10', 'B', '80/56/81', 'Dog Walking', 'Right', 'Leo', 'Tokyo', 'Cool',
+   'Ayaka Fukuhara', ' ');
+
+INSERT INTO events (id, isCurrent, isCaravan, eName, jName, begin, finish, info, t1, t2, t3, t4, t5, t6, t7, pic) VALUES
+  (1, TRUE, TRUE, 'Cinderellla Caravan', 'シンデレラキャラバン', '2016-01-13 15:00:00', '2016-01-18 15:00:00',
+   'Get Coins, Buy Idols', NULL, NULL, NULL, NULL, NULL, NULL, NULL, ''),
+  (2, FALSE, FALSE, 'LIVE Groove Dance burst', 'LIVE Groove Dance burst', '2015-12-30 15:00:00', '2016-01-08 20:59',
+   'Medley', 45128, 31182, 27954, 22930, 18874, 3032, 262, ' ');
+
+INSERT INTO cards (id, idol_id, event_id, eName, jName, rarity, type, maxLvl, awkMaxLvl, baseLife, baseVocal, baseDance, baseVisual, baseMaxLife, baseMaxVocal, baseMaxDance, baseMaxVisual, awkBaseLife, awkBaseVocal, awkBaseDance, awkBaseVisual, awkMaxLife, awkMaxVocal, awkMaxDance, awkMaxVisual, centerSkill, specialSkillName, specialSkillText, specialSkillType, baseArt, awkArt)
+VALUES
+  (1,1,NULL, );
 
 
